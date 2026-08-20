5f40b470f73c9e6f10ce667b5b724558522e8730
Fikerte Yimer <fikerteyimer06gmail.com>
2026-08-20 22:42:36 +0300

    imported the about component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./Component/Home/About";
 import Hero from "./Component/Home/Hero";
 export default function Home() {
   return (
     <div >
       
      <Hero/>
+     <About/>
       
     </div>
   );
