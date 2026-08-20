a9985b91d85be8fe69c2321c27eb773026ba80da
Fikerte Yimer <fikerteyimer06gmail.com>
2026-08-20 23:01:49 +0300

    added a subheading text with span

--- frontend/src/app/Component/Home/About.tsx
+++ frontend/src/app/Component/Home/About.tsx
@@ -4,6 +4,7 @@ const About=()=>{
             <div className="max-w-7xl mx-auto ">
                 <div className="text-center max-w-3xl mx-auto">
                     <p className="text-[#D4AF37] font-semibold tracking-widest uppercase text-sm mb-4">About AderaPay</p>
+                    <h2 className="text-4xl md:text-5xl font-bold text-[#123C2A]">Giving made simple, <span>meaningful,and secure.</span></h2>
 
                 </div>
 
