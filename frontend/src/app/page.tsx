57aa0a81e79463a1ad7fd2028f963c36cfe4f0b9
Fikerte Yimer <fikerteyimer06gmail.com>
2026-08-21 12:15:08 +0300

    imported necessary components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,18 @@
 import About from "./Component/Home/About";
 import Hero from "./Component/Home/Hero";
+import HowItWorks from "./Component/HowItWorks";
+import WhyChoose from "./Component/WhyChoose";
+import Navbar from "./Layouts/Navbar";
+import Footer from './Layouts/Footer'
 export default function Home() {
   return (
     <div >
-      
+     <Navbar/>
      <Hero/>
      <About/>
      <HowItWorks/>
+     <WhyChoose/>
+     <Footer/>
       
     </div>
   );
