3206887d1f3c6ce8aa26ce45eb3ed5012b395714
Fikerte Yimer <fikerteyimer06gmail.com>
2026-08-21 12:13:14 +0300

    added a simple language provider

---
+++ frontend/src/app/context/LanguageContext.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+type Language = "EN" | "AM";
+
+type LanguageContextType = {
+  language: Language;
+  setLanguage: (language: Language) => void;
+};
+
+const LanguageContext = createContext<LanguageContextType | undefined>(
+  undefined
+);
+
+export const LanguageProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [language, setLanguage] = useState<Language>("EN");
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+
+  if (!context) {
+    throw new Error("useLanguage must be used inside LanguageProvider");
+  }
+
+  return context;
+};
