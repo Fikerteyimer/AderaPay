dc62310de69e2723785a77d3bc46620a5ffc67dd
Fikerte Yimer <fikerteyimer06gmail.com>
2026-09-06 12:53:29 +0300

    imported the resubale welcome component

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,18 @@
+
+"use client";
+
+import Welcome from "../Component/dashboard/welcome";
+import StatCard from "../Component/dashboard/donor/StatCard";
+
+export default function DonorDashboard() {
+  return (
+    <div>
+      
+      <Welcome />
+
+    </div>
+     
+   
+  );
+}
+
